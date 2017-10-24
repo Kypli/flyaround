45e89b90d241645f008c4c090730ec6eb23ab8e4
Pierre AMBOISE <pierre.amboise@yahoo.fr>
2017-10-24 10:15:10 +0200

    Quete CRUD

---
+++ Flyaround/README.md
@@ -0,0 +1,4 @@
+Flyaround
+=========
+
+A Symfony project created on October 23, 2017, 5:21 pm.
---
+++ Flyaround/src/WCS/CoavBundle/Entity/terrain.php
@@ -0,0 +1,282 @@
+<?php
+
+namespace WCS\CoavBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * terrain
+ *
+ * @ORM\Table(name="terrain")
+ * @ORM\Entity(repositoryClass="WCS\CoavBundle\Repository\terrainRepository")
+ */
+class terrain
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=128)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="icao", type="string", length=4)
+     */
+    private $icao;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lattitude", type="float")
+     */
+    private $lattitude;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="longitude", type="float")
+     */
+    private $longitude;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adress", type="string", length=128)
+     */
+    private $adress;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=128)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="zipcode", type="string", length=16)
+     */
+    private $zipcode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=64)
+     */
+    private $country;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return terrain
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set icao
+     *
+     * @param string $icao
+     *
+     * @return terrain
+     */
+    public function setIcao($icao)
+    {
+        $this->icao = $icao;
+
+        return $this;
+    }
+
+    /**
+     * Get icao
+     *
+     * @return string
+     */
+    public function getIcao()
+    {
+        return $this->icao;
+    }
+
+    /**
+     * Set lattitude
+     *
+     * @param float $lattitude
+     *
+     * @return terrain
+     */
+    public function setLattitude($lattitude)
+    {
+        $this->lattitude = $lattitude;
+
+        return $this;
+    }
+
+    /**
+     * Get lattitude
+     *
+     * @return float
+     */
+    public function getLattitude()
+    {
+        return $this->lattitude;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param float $longitude
+     *
+     * @return terrain
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * Set adress
+     *
+     * @param string $adress
+     *
+     * @return terrain
+     */
+    public function setAdress($adress)
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    /**
+     * Get adress
+     *
+     * @return string
+     */
+    public function getAdress()
+    {
+        return $this->adress;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return terrain
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set zipcode
+     *
+     * @param string $zipcode
+     *
+     * @return terrain
+     */
+    public function setZipcode($zipcode)
+    {
+        $this->zipcode = $zipcode;
+
+        return $this;
+    }
+
+    /**
+     * Get zipcode
+     *
+     * @return string
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     *
+     * @return terrain
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
